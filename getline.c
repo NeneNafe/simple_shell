a3d9c09cbae37cc250e5e629da9f20a24ec10f55
faith2221 <edithbanda014@gmail.com>
2023-07-23 05:11:20 -0700

    history & getline

---
+++ getline.c
@@ -0,0 +1,167 @@
+#include "shell.h"
+
+/**
+ * input_buffer - Bufferschained commands.
+ * @info: Param struct.
+ * @buf: Address of buffer.
+ * @len: Address of len var.
+ * Return: Bytes read.
+ */
+
+ssize_t input_buffer(info_t *info, char **buf, size_t *len)
+{
+	ssize_t r = 0;
+	size_t len_p = 0;
+
+	if (!*len)
+	{
+		free(*buf);
+		*buf = NULL;
+		signal(SIGINT, sigintHandler);
+#if USE_GETLINE
+		r = getline(buf, &len_p, stdin);
+#else
+		r = get_getline(info, buf, &len_p);
+#endif
+		if (r > 0)
+		{
+			if ((*buf)[r - 1] == '\n')
+			{
+				(*buf)[r - 1] = '\0';
+				r--;
+			}
+			info->linecount_flag = 1;
+			rm_comments(*buf);
+			build_hist_list(info, *buf, info->histcount++);
+			{
+				*len = r;
+				info->cmd_buf = buf;
+			}
+		}
+	}
+	return (r);
+}
+
+/**
+ * get_input - Gets a line minus the newline.
+ * @info: Param struct
+ *
+ * Return: Bytes read.
+ */
+
+ssize_t get_input(info_t *info)
+{
+	static char *buf;
+	static size_t i, j, len;
+	ssize_t r = 0;
+	char **buf_p = &(info->arg), *ptr;
+
+	_putchar(BUF_FLUSH);
+	r = input_buffer(info, &buf, &len);
+	if (r == -1) /* EOF */
+		return (-1);
+	if (len)
+	{
+		j = i;
+		ptr = buf + i;
+
+		_checkChain(info, buf, &j, i, len);
+		while (j < len)
+		{
+			if (_isChain(info, buf, &j))
+				break;
+			j++;
+		}
+		i = j + 1;
+		if (i >= len)
+		{
+			i = len = 0;
+			info->cmd_buf_type = CMD_NORM;
+		}
+		*buf_p = ptr;
+		return (_strlen(ptr));
+	}
+	*buf_p = buf;
+	return (r);
+}
+
+/**
+ * read_buffer - Reads a buffer.
+ * @info: Param struct.
+ * @buf: Buffer.
+ * @i: size
+ *
+ * Return: r
+ */
+
+ssize_t read_buffer(info_t *info, char *buf, size_t *i)
+{
+	ssize_t r = 0;
+
+	if (*i)
+		return (0);
+	r = read(info->readfd, buf, READ_BUF_SIZE);
+	if (r >= 0)
+		*i = r;
+	return (r);
+}
+
+/**
+ * get_getline - Gets the next line of input from STDIN.
+ * @info: Param struct.
+ * @ptr: Address of pointer to buffer, preallocated or NULL.
+ * @length: Size of reallocated ptr buffer if not NULL.
+ *
+ * Return: Success.
+ */
+
+int get_getline(info_t *info, char **ptr, size_t *length)
+{
+	static char buf[READ_BUF_SIZE];
+	static size_t i, len;
+	size_t k;
+	ssize_t r = 0, str = 0;
+	char *p = NULL, *new_p = NULL, *c;
+
+	p = *ptr;
+	if (p && length)
+		str = *length;
+	if (i == len)
+		i = len = 0;
+	r = read_buffer(info, buf, &len);
+	if (r == -1 || (r == 0 && len == 0))
+		return (-1);
+
+	c = _strchr(buf + i, '\n');
+	k = c ? 1 + (unsigned int)(c - buf) : len;
+	new_p = our_realloc(p, str, str ? str + k : k + 1);
+	if (!new_p) /* MALLOC FAILURE */
+		return (p ? free(p), -1 : -1);
+
+	if (str)
+		_strncat(new_p, buf + i, k - i);
+	else
+		_strncpy(new_p, buf + i, k - i + 1);
+	str += k - i;
+	i = k;
+	p = new_p;
+
+	if (lenght)
+		*length = str;
+	*ptr = p;
+	return (str);
+}
+
+/**
+ * sigintHandler - Blocks ctrl-C
+ * @sig_num: The signal number.
+ *
+ * Return: void.
+ */
+
+void sigintHandler(__attribute__((unused))int sig_num)
+{
+	_puts("\n");
+	_puts("$ ");
+	_putchar(BUF_FLUSH);
+}
---
+++ hist.c
@@ -0,0 +1,144 @@
+#include "shell.h"
+
+/**
+ * get_hist_file - Gets the history file.
+ * @info: Parameter struct.
+ *
+ * Return: Allocated string containing hisstory file.
+ */
+
+char *get_hist_file(info_t *info)
+{
+	char *buf, *dir;
+
+	dir = more_getenv(info, "HOME=");
+	if (!dir)
+		return (NULL);
+	buf = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
+	if (!buf)
+		return (NULL);
+	buf[0] = 0;
+	_strcpy(buf, dir);
+	_strcat(buf, "/");
+	_strcat(buf, HIST_FILE);
+	return (buf);
+}
+
+/**
+ * write_hist - Creates a file, or appends to an existing file.
+ * @info: The parameter struct.
+ *
+ * Return: 1 on success, else -1.
+ */
+
+int write_hist(info_t *info)
+{
+	ssize_t fd;
+	char *filename = get_hist_file(info);
+	list_t *node = NULL;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
+	free(filename);
+	if (fd == -1)
+		return (-1);
+	for (node = info->history; node; node = node->next)
+	{
+		_nputsfd(node->str, fd);
+		our_putfd('\n', fd);
+	}
+	our_putfd(BUF_FLUSH, fd);
+	close(fd);
+	return (1);
+}
+
+/**
+ * read_hist - Reads history from file.
+ * @info: The parameter struct.
+ *
+ * Return: Histcounton success, 0 otherwise.
+ */
+
+int read_hist(info_t *info)
+{
+	int i, last = 0, linecount = 0;
+	ssize_t fd, rdlen, fsize = 0;
+	struct stat st;
+	char *buf = NULL, *filename = get_hist_file(info);
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	free(filename);
+	if (fd == -1)
+		return (0);
+	if (!fstat(fd, &st))
+		fsize = st.st_size;
+	if (fsize < 2)
+		return (0);
+	buf = malloc(sizeof(char) * (fsize + 1));
+	if (!buf)
+		return (0);
+	rdlen = read(fd, buf, fsize);
+	buf[fsize] = 0;
+	if (rdlen <= 0)
+		return (free(buf), 0);
+	close(fd);
+	for (i = 0; i < fsize; i++)
+		if (buf[i] == '\n')
+		{
+			buf[i] = 0;
+			build_hist_list(info, buf + last, linecount++);
+			last = i + 1;
+		}
+	if (last != i)
+		build_hist_list(info, buf + last, linecount++);
+	free(buf);
+	info->histcount = linecount;
+	while (info->histcount-- >= HIST_MAX)
+		delete_node(&(info->history), 0);
+	renumber_hist(info);
+	return (info->histcount);
+}
+
+/**
+ * build_hist_list - Adds entry to a history linked list.
+ * @info: Structure containing potential arguments used to maintain.
+ * @buf: Buffer.
+ * @linecount: The history linecount, histcount
+ *
+ * Return: Always 0.
+ */
+int build_hist_list(info_t *info, char *buf, int linecount)
+{
+	list_t *node = NULL;
+
+	if (info->history)
+		node = info->history;
+	add_node_end(&node, buf, linecount);
+	if (!info->history)
+		info->history = node;
+	return (0);
+}
+
+/**
+ * renumber_hist - Renumbers the history linked list after changes.
+ * @info: Structure containing potential arguments used to maintain.
+ *
+ * Return: The new histcount.
+ */
+
+int renumber_hist(info_t *info)
+{
+	list_t *node = info->history;
+	int j = 0;
+
+	while (node)
+	{
+		node->num = j++;
+		node = node->next;
+	}
+	return (info->histcount = j);
+}
+
